6e54a087d9702a1fac3ab0db6eb15cdf5c52c921
DagemBefikadu <befikadu.dagem@gmail.com>
2021-11-09 13:57:15 -0800

    Up to date code that works

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,5 +1,6 @@
 let express = require('express')
 let db = require('../models')
+const article = require('../models/article')
 let router = express.Router()
 
 // POST /articles - create a new post
@@ -32,16 +33,34 @@ router.get('/new', (req, res) => {
 router.get('/:id', (req, res) => {
   db.article.findOne({
     where: { id: req.params.id },
-    include: [db.author]
+    include: [db.author, db.comment]
   })
   .then((article) => {
     if (!article) throw Error()
-    console.log(article.author)
+    console.log('this is the article\n', article)
     res.render('articles/show', { article: article })
   })
   .catch((error) => {
     console.log(error)
     res.status(400).render('main/404')
+  })
+})
+
+//POST ==> /articles/:id:comments --> this willl add a new comment
+//req.body is coming from the form in show.ejs
+//req.params is referring the url
+router.post('/:id/comments', (req,res) => {
+  db.comment.create({
+    name: req.body.name,
+    content: req.body.content,
+    articleId: req.params.id
+  })
+  .then(resPost => {
+    console.log('created comment\n', resPost)
+    res.redirect(`/articles/${req.params.id}`)
+  })
+  .catch(err => {
+    res.status(404).render('main/404')
   })
 })
 
